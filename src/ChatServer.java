61a80b6550c79de8a748cf3a73afb778643bf02f
showeye <tadeusz.jargilo@gmail.com>
2015-05-06 14:53:22 +0200

    second commit

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,4 +1,47 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
 
 public class ChatServer {
+	static Vector ClientSockets;
+	static Vector LoginNames;
+
+	ChatServer() throws IOException {
+		ServerSocket server = new ServerSocket(5217);
+		ClientSockets = new Vector();
+		LoginNames = new Vector();
+		
+		while(true) {
+			Socket client = server.accept();
+			AcceptClient acceptClient = new AcceptClient(client);
+		}
+	}
+	
+	class AcceptClient extends Thread {
+		Socket ClientSocket;
+		DataInputStream din;
+		DataOutputStream dout;
+		AcceptClient(Socket client) throws IOException {
+			ClientSocket = client;
+			din = new DataInputStream(ClientSocket.getInputStream());
+			dout = new DataOutputStream(ClientSocket.getOutputStream());
+			
+			String LoginName = din.readUTF();
+			
+			LoginNames.addElement(LoginName);
+			ClientSockets.add(ClientSocket);
+			
+			start();
+		}
+		
+		public void run() {
+			while(true) {
+				
+			}
+		}
+	}
 
 }
